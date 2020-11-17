9217c3bd4b758f4e7b56ea90c364da4d97a3c9dc
mary9026 <mary9026@naver.com>
2020-11-17 12:58:08 +0900

    선생님이 작성한 성적V2

--- src/mary/SungJukV2.java
+++ src/mary/SungJukV2.java
@@ -19,46 +19,56 @@ import java.util.Scanner;
 public class SungJukV2 {
     public static void main(String[] args) {
        // 변수선언
+       String name = "지현";
+       int kor = 99;
+       int eng = 98;
+       int mat = 99;
+       int sum = 0;
+       double mean = 0.0;
+       char grd ='가';
 
-
-
-       Scanner scanner = new Scanner(System.in);
-       System.out.println("이름을 입력하세요 : ");
-       String name = scanner.nextLine();
-       System.out.println("국어 점수를 입력하세요 : ");
-       int kor = scanner.nextInt();
-       System.out.println("영어 점수를 입력하세요 : ");
-       int eng = scanner.nextInt();
-       System.out.println("수학 점수를 입력하세요 : ");
-       int mat = scanner.nextInt();
-       System.out.println("총점은" + (kor+eng+mat) + "입니다.");
-       int grd = scanner.nextInt();
 
 
        // 처리
-      switch(grd/10) {
-         case 10 :
-         case 9 :
-            System.out.println("학점은 수 입니다.");
-            break;
-         case 8 :
-            System.out.println("학점은 우 입니다.");
-            break;
-         case 7 :
-            System.out.println("학점은 미 입니다.");
-            break;
-         case 6 :
-            System.out.println("학점은 양 입니다.");
-            break;
-         case 5 : case 4 : case 3 : case 2 : case 1 : case 0 :
-            System.out.println("학점은 가 입니다.");
-            break;
+       // 성적 데이터를 키보드로 입력받기 위해 Scanner 클래스 초기화
+       Scanner scanner = new Scanner(System.in);
+       System.out.print("이름을 입력하세요 : ");
+       name = scanner.nextLine();
+       System.out.print("국어 점수를 입력하세요 : ");
+       kor = scanner.nextInt();
+       System.out.print("영어 점수를 입력하세요 : ");
+       eng = scanner.nextInt();
+       System.out.print("수학 점수를 입력하세요 : ");
+       mat = scanner.nextInt();
 
+        sum = kor+eng+mat;
+        mean = (double)sum / 3;
+
+        //학점 계산은 switch 문으로 처리
+        //switch()괄호 안에는 정수 또는 문자타입이 올 수 있음
+
+      switch((int)mean/10) {
+         case 10 : grd = '수'; break;
+         case 9 : grd = '수'; break;
+         case 8 : grd = '우'; break;
+         case 7 : grd = '미'; break;
+         case 6 : grd = '양'; break;
+        default : grd = '가';
       }
 
-
+      // String.format(형식지정자, 변수들)
+        mean = Double.parseDouble(String.format("%.1f", mean));
 
        // 결과출력
+       System.out.println("이름 : " + name);
+       System.out.println("국어 : " + kor);
+       System.out.println("영어 : " + eng);
+       System.out.println("수학 : " + mat);
+       System.out.println("-------------------");
+       System.out.println("총점 : " + sum);
+       System.out.println("평균 : " + mean);
+       //System.out.printf("평균 : %.1f\n", mean);
+       System.out.println("학점 : " + grd);
 
 
 
